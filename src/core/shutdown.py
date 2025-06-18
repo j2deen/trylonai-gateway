f2e27def17e6328566e1d9f6d446e21a92d1be1d
tokenizer-decode <kbuyukakyuz@gmail.com>
2025-06-18 16:11:43 +0300

    add type annotation to shutdown manager

--- src/core/shutdown.py
+++ src/core/shutdown.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 class ShutdownManager:
     """Simplified ShutdownManager, primarily for potential future resource management."""
 
-    def __init__(self):
+    def __init__(self) -> None:
         pass
 
     async def cleanup(self) -> None:
